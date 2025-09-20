d66bd86cf41c2e24ea07b957a393f5c44825657a
Lenovo User <Lenovo User@DESKTOP-2VDT7AQ>
2025-09-19 18:54:20 -0700

    Add project files.

---
+++ PracticaGithub/OperacionesHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaGithub
+{
+    internal class OperacionesHelper
+    {
+        public static void SumaGithub (int n1, int n2)
+        {
+            Console.Write(n1 + n2);
+        }
+    }
+}
